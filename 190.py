4f5e6d83912841088e517dfc5d1f7b8db73751a8
lx <wowfunny@163.com>
2021-10-04 16:08:14 +0800

    add new

---
+++ 190.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        def Sort():
+
+            print("ok")
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+if __name__ == '__main__':
+    s='00000010100101000001111010011100'
+
+    print(int(reversed(s)))
