9a2ca4adaad355f77ced543405983071577b65f3
lx <wowfunny@163.com>
2021-10-12 16:58:01 +0800

    test version for 148

--- 148.py
+++ 148.py
@@ -1,8 +1,9 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution(object):
     def sortList(self, head):
         """
@@ -10,8 +11,44 @@ class Solution(object):
         :rtype: ListNode
         """
         arr = []
-        while head.next is not None:
-            arr.append(head.val)
-            head=head.next
+        cur= head
+        while cur.next is not None:
+            cur = cur.next
+            arr.append(cur.val)
+
         arr.sort()
-        print(arr)
+        i =0
+        curs = head
+        while curs.next is not None:
+            curs = curs.next
+            curs.val = arr[i]
+            i+=1
+        return head
+
+def printLink(head):
+    result =[]
+    curs=head
+    while curs.next is not None:
+        curs = curs.next
+        result.append(curs.val)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    arr = [4,2,1,3]
+    head = ListNode()
+    cur=head
+    for value in arr:
+        print("appending----"+str(value))
+        newNode = ListNode(val=value)
+
+        cur.next=newNode
+        cur = cur.next
+
+    printLink(head)
+    su = Solution()
+    newhead = su.sortList(head)
+    printLink(newhead)
+
+
