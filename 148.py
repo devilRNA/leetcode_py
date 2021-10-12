0bfae18cd35c6f569544ec942ad38f452abc6464
lx <wowfunny@163.com>
2021-10-12 17:23:18 +0800

    leetcode version for 148

--- 148.py
+++ 148.py
@@ -4,30 +4,51 @@ class ListNode(object):
         self.val = val
         self.next = next
 
-class Solution(object):
-    def sortList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        arr = []
-        cur= head
-        while cur.next is not None:
-            cur = cur.next
-            arr.append(cur.val)
 
-        arr.sort()
-        i =0
-        curs = head
-        while curs.next is not None:
-            curs = curs.next
-            curs.val = arr[i]
-            i+=1
-        return head
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        def sortFunc(head: ListNode, tail: ListNode) -> ListNode:
+            if not head:
+                return head
+            if head.next == tail:
+                head.next = None
+                return head
+            slow = fast = head
+            while fast != tail:
+                slow = slow.next
+                fast = fast.next
+                if fast != tail:
+                    fast = fast.next
+            mid = slow
+            return merge(sortFunc(head, mid), sortFunc(mid, tail))
+
+        def merge(head1: ListNode, head2: ListNode) -> ListNode:
+            dummyHead = ListNode(0)
+            temp, temp1, temp2 = dummyHead, head1, head2
+            while temp1 and temp2:
+                if temp1.val <= temp2.val:
+                    temp.next = temp1
+                    temp1 = temp1.next
+                else:
+                    temp.next = temp2
+                    temp2 = temp2.next
+                temp = temp.next
+            if temp1:
+                temp.next = temp1
+            elif temp2:
+                temp.next = temp2
+            return dummyHead.next
+
+        return sortFunc(head, None)
+
+
+
+
+
 
 def printLink(head):
-    result =[]
-    curs=head
+    result = []
+    curs = head
     while curs.next is not None:
         curs = curs.next
         result.append(curs.val)
@@ -36,19 +57,16 @@ def printLink(head):
 
 
 if __name__ == '__main__':
-    arr = [4,2,1,3]
+    arr = [4, 2, 1, 3]
     head = ListNode()
-    cur=head
+    cur = head
     for value in arr:
-        print("appending----"+str(value))
+        print("appending----" + str(value))
         newNode = ListNode(val=value)
 
-        cur.next=newNode
+        cur.next = newNode
         cur = cur.next
-
     printLink(head)
     su = Solution()
     newhead = su.sortList(head)
     printLink(newhead)
-
-
